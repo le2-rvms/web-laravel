754535b32db3a9be0f56db22bd75f4c0b31c511b
蘇 <su@le2.ltd>
2026-03-12 20:44:58 +0800

    update

--- app/Enum/Iot/EventType_CMD.php
+++ app/Enum/Iot/EventType_CMD.php
@@ -9,8 +9,11 @@ class EventType_CMD extends EnumLikeBase
     public const string CMD     = 'cmd';
     public const string CMD_ACK = 'cmd-ack';
 
+    public const string BLE_ACK = 'evt';
+
     public const array LABELS = [
         self::CMD     => '命令下发',
         self::CMD_ACK => '命令执行',
+        self::BLE_ACK => 'BLE命令执行',
     ];
 }
