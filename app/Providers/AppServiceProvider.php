134b6214f1cecd65c75eefbde890a630d29757aa
蘇 <su@le2.ltd>
2026-03-16 14:12:49 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use App\Models\_\PersonalAccessToken;
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 
@@ -27,13 +24,6 @@ class AppServiceProvider extends ServiceProvider
         foreach (glob(base_path().'/helpers/*.php') as $filename) {
             require_once $filename;
         }
-
-        RateLimiter::for('emqx-auth', function (Request $request) {
-            $ip       = (string) $request->ip();
-            $username = (string) $request->input('username', '');
-
-            return Limit::perMinute(60)->by($ip.'|'.$username);
-        });
 
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
--- routes/api_iot.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\Iot\Mqtt\EmqxAuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/emqx/auth', [EmqxAuthController::class, 'authenticate'])->middleware('throttle:emqx-auth');
