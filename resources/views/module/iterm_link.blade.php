dd4dc77f895c271da1c2c15bfdd7bf566f56396a
蘇 <su@le2.ltd>
2026-03-27 14:50:40 +0800

    update

--- resources/views/module/iterm_link.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-    <div class="col-sm-12 col-md-5">
-        全 {{ $data->total() }} 条
-        @if($data->total())
-            显示： {{ $data->firstItem() }} ～ {{ $data->lastItem() }}
-        @endif
-    </div>
-    <div class="col-sm-12 col-md-7 d-flex flex-row-reverse ">{{ $data->links() }}</div>
-</div>
