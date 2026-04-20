3b438f601e69dd887b2484458df02a4f43204df2
蘇 <su@le2.ltd>
2026-04-20 23:38:39 +0800

    update

--- script/php/setup.sh
+++ script/php/setup.sh
@@ -12,14 +12,14 @@ composer config -g --list | grep repositories
 
 php -v
 composer --version
+php artisan about
+php artisan --version
 
 APP_ENV="${APP_ENV:-dev}"
 
 if [ "$APP_ENV" = "production" ] || [ "$APP_ENV" = "staging" ]; then
     echo "执行生产环境部署..."
     composer install --prefer-dist --no-interaction --no-progress --no-dev --optimize-autoloader --classmap-authoritative
-    php artisan about
-    php artisan --version
 #    php artisan optimize:clear
     php artisan config:cache
     php artisan route:cache
@@ -30,7 +30,5 @@ else
     # composer config github-protocols https
      composer update -vvv
 #    composer install
-    php artisan --version
-    php artisan about
 #    php artisan optimize:clear  # 这个时候数据表还没有准备好
 fi
